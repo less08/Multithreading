aead8d138785cf926e117aded03066da2e8d8aa8
Elena Samsonova <elenasamsonova08@gmail.com>
2021-02-24 14:33:27 +0200

    added excutor and fork join calculators

---
+++ src/main/java/com/mtt/ForkJoinTask.java
@@ -0,0 +1,43 @@
+package com.mtt;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+class ForkJoinTask extends RecursiveTask<Integer> {
+    private int threshHold;
+    private List<Integer> list;
+
+    public ForkJoinTask(List<Integer> list) {
+        this.list = list;
+        this.threshHold = list.size();
+    }
+
+    private ForkJoinTask(List<Integer> list, int threshHold) {
+        this.list = list;
+        this.threshHold = threshHold;
+    }
+
+    @Override
+    public Integer compute() {
+        if (list.size() > threshHold) {
+            return ForkJoinTask.invokeAll(createSubtasks())
+                .stream()
+                .mapToInt(ForkJoinTask::join)
+                .sum();
+        }
+        return processing(list);
+    }
+
+    private Collection<ForkJoinTask> createSubtasks() {
+        List<ForkJoinTask> dividedTasks = new ArrayList<>();
+        dividedTasks.add(new ForkJoinTask(list.subList(0, list.size() / 2), threshHold));
+        dividedTasks.add(new ForkJoinTask(list.subList(list.size() / 2, list.size()), threshHold));
+        return dividedTasks;
+    }
+
+    private Integer processing(List<Integer> values) {
+        return values.stream().reduce(0, Integer::sum);
+    }
+}
